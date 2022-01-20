90f2cfa01c2df4153a7f45ac956210f3016e37f7
viniciusnovais74 <viniciusnovais74@gmail.com>
2022-01-20 17:08:46 -0300

    Primeiros componentes (Arrow Function)

--- React-Legado/src/index.jsx
+++ React-Legado/src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Olá</h1>,document.getElementById('app'))
+import Componente from './componente'
+ReactDOM.render(<Componente/>,document.getElementById('app'))
 
